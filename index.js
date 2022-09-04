538fc0295bce2e4d6b43c4963b77f48391d2a6c8
kelicaleb <kelicaleb7@gmail.com>
2022-09-04 15:12:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.querySelector("main").remove("id")
+console.log(main);
+var newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory" )
+newHeader.innerHTML = "H1"
+document.body.append(newHeader)
+var newHeader2 = document.createElement("h1");
+newHeader2.innerHTML = "Victory";
+newHeader.append(newHeader2)
+var champion = document.createElement("h1");
+newHeader2.innerHTML = "Caleb is the champion";
+newHeader.append(champion)
